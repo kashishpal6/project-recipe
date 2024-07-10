91758ae24be9583d2c4cc9fcb523f2905f0bf7c4
kashishpal6 <kashishpal386@gmail.com>
2024-07-10 17:17:09 +0530

    login page

--- recipe/urls.py
+++ recipe/urls.py
@@ -17,21 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from vege.views import *
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recipe/',recipes,name="recipesssss"),
-    path('delete-recipe/<id>/',delete_recipe,name="delete_recipe"),
-    path('update-recipe/<id>/',update_recipe,name="update_recipe"),
+    path('recipe/', recipes, name="recipes"),
+    path('delete-recipe/<int:id>/', delete_recipe, name="delete_recipe"),
+    path('update-recipe/<int:id>/', update_recipe, name="update_recipe"),
+    path('login/' , login_page, name="login_page"),
+    path('register/' , register, name="register"),
 ]
-    
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += staticfiles_urlpatterns()
+
 
 
--- vege/views.py
+++ vege/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
+
 
 # Create your views here.
 def recipes(request):
@@ -35,7 +39,7 @@ def update_recipe(request , id):
        queryset.recipe_image = recipe_image
 
       queryset.save()
-      return redirect('/recipesssss/')
+      return redirect()
          
      context = {'recipesssss': queryset}
 
@@ -48,6 +52,46 @@ def delete_recipe(request , id):
      queryset = Recipe.objects.get(id=id)
      queryset.delete()
      return redirect('/recipesssss/')
+
+def login_page(request):
+   if request.method== "POST":
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      if not User.objects.filter(username=username).exists():
+         messages.error(request,'Invalid Username')
+         return redirect('/login/')
+      user = authenticate(username=username,password =password)
+      if user is None:
+          messages.error(request,'Invalid Password')
+          return redirect('/login/')
+      else: 
+          login(request,user)
+          return redirect ('/recipe/')
+   return render(request,'login.html')
+def register(request):
+   if request.method== "POST":
+      first_name = request.POST.get('first_name')
+      last_name = request.POST.get('last_name')
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      user =User.objects.filter(username= username)
+      if user.exists():
+         messages.error(request,'Username already exists')
+         return redirect('/register/')
+
+      user =User.objects.create(
+         first_name = first_name,
+         last_name =  last_name,
+         username =  username)
+      user.set_password(password)
+      user.save()
+      messages.error(request, 'Account created succesfully')
+      return redirect('/register/')
+
+
+
+   return render(request,'register.html')
+
      
 
 
